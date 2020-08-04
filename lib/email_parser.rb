b05ab52c1436c9d5ab4ab2acde3d13aca1042e88
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 02:42:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   
   #need to create a method called parse that can separate the string my space or comma
   def parse
-    array_of_emails = @string_of_emails.split("," || "")
+    array_of_emails = @string_of_emails.split(" ")
     binding.pry
   end
 end
